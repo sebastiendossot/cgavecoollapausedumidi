baf9393b1ce77a7d9935ae9997c581b7640d4154
sebastiendossot <sebastien.dossot@gmail.com>
2020-11-15 22:13:25 +0100

    trying to fix the thing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :team_id
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:team_id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   post '/home/vote' => 'home#vote'
   post '/home/simulation_vote' => 'home#simulation_vote'
   
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
